82e5d91ea7a45d9de34fa963128ae1099f13c479
ren <kikuren1st@gmail.com>
2024-04-10 17:40:09 +0900

    practice

---
+++ fib_for.py
@@ -0,0 +1,19 @@
+#フィボナッチ数列を求める関数
+def fib_list(n):
+    a,b=1,1
+    result=[a,b]
+    #最初は1，1から始める
+    for i in range(1,n-1):
+        c=a+b#次の値を求める
+        result.append(c)
+        a,b=b,c#aとbの値を更新
+    return result
+
+#テスト
+def test_fib_list():
+    assert fib_list(3)==[1,2,3]
+    assert fib_list(8)==[1,1,2,3,5,8,13,21]
+    
+if __name__=='__main__':
+    #フィボナッチ数列を11個表示する
+    print(fib_list(11))
