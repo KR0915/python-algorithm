f2550de423fca2d9feae3a25eeb4af72acb55e5f
ren <kikuren1st@gmail.com>
2024-05-04 18:22:34 +0900

    practice

--- B - Substring.py
+++ B - Substring.py
@@ -1,5 +1,8 @@
 S=input()
-
+ans=0
 for i in range(len(S)):
-    for j in range(i):
-        S[]
+    get=set()
+    for j in range(len(S)-i):
+        get.add(S[i:j])
+    ans+=len(get)
+print(ans)
