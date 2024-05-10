d33412b70f37230026e07b6f74d2c20742850757
ren <kikuren1st@gmail.com>
2024-05-10 15:45:39 +0900

    practice

--- B - Which is ahead.py
+++ B - Which is ahead.py
@@ -1,20 +1,7 @@
 N=int(input())
-A=[]
-for t in range(N):
-    v=input()
-    A.append(v)
-Q=input()
-a=[]
-b=[]
-for i in range(Q):
+P=list(map(int,input().split()))
+Q=int(input())
+for _ in range(Q):
     A,B=map(int,input().split())
-    for j in range(N):
-        if A[j]==A:
-            a.append(j)
-        elif A[j]==B:
-            b.append(j)
-            
-        
-print(a)
-print(b)
+    print(A if P.index(A)<P.index(B) else B)
     
--- test.py
+++ test.py
@@ -1,20 +1,10 @@
-# 入力された値（すべてstr型）をリストに格納します
-input_values = ["1", "2", "3", "a", "b", "c", "4", "5"]
+N=int(input())
+P=list(map(int,input().split()))
+Q=int(input())
+for _ in range(Q):
+    A,B=map(int,input().split())
+    print(P.index(A))
+    
 
-# int型に変換できる値とそうでない値を格納するためのリストを作成します
-convertible_to_int = []
-not_convertible_to_int = []
-
-# 値をint型に変換し、変換できるかどうかを確認します
-for value in input_values:
-    try:
-        converted_value = int(value)
-        convertible_to_int.append(converted_value)
-    except ValueError:
-        not_convertible_to_int.append(value)
-
-# 結果を出力します
-print("int型に変換できる値:", convertible_to_int)
-print("int型に変換できない値:", not_convertible_to_int)
 
 
