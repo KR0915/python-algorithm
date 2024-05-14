c721f40ee2f08928e468a617a4b5b0ccd79993b8
ren <kikuren1st@gmail.com>
2024-05-15 00:07:06 +0900

    practice

--- B - Extended ABC.py
+++ B - Extended ABC.py
@@ -1,9 +1,10 @@
 S=input()
-for i in range(len(S)-1):
-    if (S[i]+S[i+1])=='AC' or (S[i]+S[i+1])=='BA' or (S[i]+S[i+1])=='CA' or (S[i]+S[i+1])=='CB':
-        print('No')
-        break
-else:
+a=S.count('A')
+b=S.count('B')
+c=S.count('C')
+if S==('A'*a+'B'*b+'C'*c):
     print('Yes')
+else:
+    print('No')
         
         
