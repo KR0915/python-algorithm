3013fcba1fb849c81978d4f2657e3433f67f0b1c
ren <kikuren1st@gmail.com>
2024-07-08 11:43:08 +0900

    practice

---
+++ B - Intesection of Cuboids.py
@@ -0,0 +1,22 @@
+a, b, c, d, e, f = map(int, input().split())
+g, h, i, j, k, l = map(int, input().split())
+
+x1_min, x1_max = min(a, d), max(a, d)
+y1_min, y1_max = min(b, e), max(b, e)
+z1_min, z1_max = min(c, f), max(c, f)
+
+x2_min, x2_max = min(g, j), max(g, j)
+y2_min, y2_max = min(h, k), max(h, k)
+z2_min, z2_max = min(i, l), max(i, l)
+
+x=max(0, min(x1_max, x2_max) - max(x1_min, x2_min))
+y=max(0, min(y1_max, y2_max) - max(y1_min, y2_min))
+z=max(0, min(z1_max, z2_max) - max(z1_min, z2_min))
+
+vol=x*y*z
+
+if vol>0:
+    print('Yes')
+else:
+    print('No')
+
