0f9a3e57245bf9e0d38c53d55887208eb54b1145
ren <kikuren1st@gmail.com>
2024-07-10 10:10:09 +0900

    practice

--- tempCodeRunnerFile.py
+++ tempCodeRunnerFile.py
@@ -1,8 +1,15 @@
-A,B=map(int,input().split())
-i=0
-if B==1:
-    i=0
-else:
-    while ((A*i)-(i-1))<B:
-        i+=1
-print(i)
+N,K=map(int,input().split())
+A=list(map(int,input().split()))
+
+A.sort()
+    
+min_diff = float('inf')
+
+for start in range(K + 1):
+    end = N - (K - start)
+    if end <= N:
+        current_min = A[start]
+        current_max = A[end - 1]
+        min_diff = min(min_diff, current_max - current_min)
+        
+print(min_diff)
