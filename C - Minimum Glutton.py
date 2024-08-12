5091b5279c80a74224c6c7e5b0de60189645f0d0
ren <kikuren1st@gmail.com>
2024-08-12 21:01:22 +0900

    practice

--- C - Minimum Glutton.py
+++ C - Minimum Glutton.py
@@ -1,4 +1,18 @@
 N,X,Y=map(int,input().split())
-A=input()
-B=input()
-print(N,X,Y,A,B)
+A=list(map(int,input().split()))
+B=list(map(int,input().split()))
+A.sort()
+B.sort()
+sum_A=0
+sum_B=0
+flag=True
+for i in range(1,N+1):
+    sum_A+=A[-i]
+    sum_B+=B[-i]
+    if sum_A>X or sum_B>Y:
+        print(i)
+        flag=False
+        break
+    
+if flag:
+    print(N)
