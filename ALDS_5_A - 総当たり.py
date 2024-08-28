08a699640b878070a5a7fef9c7adb97fafd9fa4a
ren <kikuren1st@gmail.com>
2024-08-28 19:14:54 +0900

    practice

--- ALDS_5_A - 総当たり.py
+++ ALDS_5_A - 総当たり.py
@@ -2,10 +2,19 @@ n=int(input())
 A=list(map(int,input().split()))
 q=int(input())
 M=list(map(int,input().split()))
-
+total=set()
 for i in range(1<<n):
     subset=[]
+    subset_sum = 0
     for j in range(n):
         if (i>>j)&1:
             subset.append(A[j])
-    print(subset)
+            subset_sum += A[j]
+            total.add(subset_sum)
+    # print(total)
+
+for m in M:
+    if m in total:
+        print("yes")
+    else:
+        print("no")
