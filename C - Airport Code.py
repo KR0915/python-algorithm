dd2dc816b6d9924953f2a12330fbc2ccd83ec1b5
ren <kikuren1st@gmail.com>
2024-10-26 13:27:28 +0900

    commit

--- C - Airport Code.py
+++ C - Airport Code.py
@@ -1,30 +1,24 @@
-S=input()
-T=input()
-t=T.lower()
-flag1=False
-flag2=False
-flag3=False
-if t[-1]=='x':
-    for i in range(len(S)):
-        if t[0]==S[i]:
-            flag1=True
-        elif t[1]==S[i]:
-            flag2=True
-    else:
-        if flag1 and flag2:
-            print('Yes')
-        else:
-            print('No')
-elif t[-1]!='x':
-    for i in range(len(S)):
-        if t[0]==S[i]:
-            flag1=True
-        elif t[1]==S[i]:
-            flag2=True
-        elif t[2]==S[i]:
-            flag3=True
-    else:
-        if flag1 and flag2 and flag3:
-            print('Yes')
-        else:
-            print('No')
+S = input()
+T = input().lower()
+flag = 0
+
+if T[-1:] == 'x':
+    T = T[:-1]
+
+    for i in S:
+        if i == T[flag]:
+            if flag >= 1:
+                print('Yes')
+                exit()
+            else:
+                flag += 1     
+else:
+    for i in S:
+        if i == T[flag]:
+            if flag >= 2:
+                print('Yes')
+                exit()
+            else:
+                flag += 1
+        
+print('No')
--- D - ABA.py
+++ D - ABA.py
@@ -3,11 +3,9 @@ def main():
     N = len(S)
     
     sum = [[0] * (N + 1) for _ in range(26)]
-    print(sum)
     for i in range(N):
         for j in range(26):
             sum[j][i + 1] = sum[j][i]
-            print(sum[j][i + 1], sum[j][i])
         sum[ord(S[i]) - ord('A')][i + 1] += 1
 
     ans = 0
@@ -15,9 +13,7 @@ def main():
         for j in range(26):
             l = sum[j][i]
             r = sum[j][N] - sum[j][i + 1]
-            print(l,r)
             ans += l * r
-
     print(ans)
 
 if __name__ == "__main__":
