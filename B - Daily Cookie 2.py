c4c1b131a986e3db072452d911647232abb847db
ren <kikuren1st@gmail.com>
2024-12-04 18:14:39 +0900

    commit

---
+++ B - Daily Cookie 2.py
@@ -0,0 +1,11 @@
+N, D = map(int, input().split())
+S = list(input())
+
+for _ in range(D):
+    for i in range(N - 1, -1, -1):
+        if S[i] == '@':
+            S[i] = '.'
+            break
+
+
+print(''.join(S))
