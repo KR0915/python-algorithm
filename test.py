11a7e1190eb5987239230af63974d6d16f8357c1
ren <kikuren1st@gmail.com>
2024-12-08 16:30:54 +0900

    commit

--- test.py
+++ test.py
@@ -1,27 +1,30 @@
-def max_11_22_substring_length(S):
-    N = len(S)
-    max_length = 0
+from bisect import bisect_left
 
-    for left in range(N):
-        if S[left] == '1':
-            right = left
-            while right < N and S[right] == '1':
-                right += 1
-            if right < N and S[right] == '/':
-                mid = right
-                right += 1
-                while right < N and S[right] == '2':
-                    right += 1
-                if right - left == 2 * (mid - left) + 1:
-                    max_length = max(max_length, right - left)
-        left += 1
+def solve():
+    import sys
+    input = sys.stdin.read
+    data = input().split()
+    
+    N, M = int(data[0]), int(data[1])
+    A = list(map(int, data[2:N+2]))
+    B = list(map(int, data[N+2:]))
+    
+    # 美食度と人のインデックスをペアにしてソート
+    people = sorted((a, i + 1) for i, a in enumerate(A))
+    
+    result = []
+    for b in B:
+        # 二分探索で条件を満たす最初の人を探す
+        idx = bisect_left(people, (b, 0))
+        if idx < len(people) and people[idx][0] <= b:
+            # 寿司を取る人を確定
+            result.append(people[idx][1])
+            people.pop(idx)  # 人を削除（その人はもう寿司を取れない）
+        else:
+            # 誰も寿司を取れない場合
+            result.append(-1)
+    
+    sys.stdout.write('\n'.join(map(str, result)) + '\n')
 
-    return max_length
-
-# 入力の読み込み
-N = int(input())
-S = input()
-
-# 最大部分文字列の長さを求める
-result = max_11_22_substring_length(S)
-print(result)
+# 実行
+solve()
